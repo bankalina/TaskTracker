5a2e4fded80871b4413a02a33d01706281c6737b
Kalina Baniak <kalina.baniak@student.pk.edu.pl>
2025-09-06 22:32:55 +0200

    Implement database operations and verify user existence during login

--- src/controllers/SecurityController.php
+++ src/controllers/SecurityController.php
@@ -2,30 +2,37 @@
 
 require_once 'AppController.php';
 require_once __DIR__.'/../models/User.php';
+require_once __DIR__.'/../repository/UserRepository.php';
 
 class SecurityController extends AppController 
 {
     public function login()
     {
-        # docelowo bedziemy pobierac uzytkownikow z bazy
-        $user = new User("jsnow@pk.edu.pl", 'admin', 'John', 'Snow');
+        $userRepository = new UserRepository();
 
-        if($this->isPost()) {
-            $email = $_POST["email"];
-            $password = $_POST["password"];
-
-            if ($user->getEmail() !== $email) {
-                return $this->render('login', ['messages' => ['User with this email doesnt exist!!!']]);
-            }
-
-            if ($user->getPassword() !== $password) {
-                return $this->render('login', ['messages' => ['Wrong password!']]);
-            }
-
-            return $this->render('tasks');
+        if (!$this->isPost()) {
+            return $this->render('login');
         }
 
-        return $this->render('login');
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+
+        $user = $userRepository->getUser($email);
+
+        if (!$user) {
+            return $this->render('login', ['messages' => ['User not found!']]);
+        }
+
+        if ($user->getEmail() !== $email) {
+            return $this->render('login', ['messages' => ['User with this email not exist!']]);
+        }
+
+        if ($user->getPassword() !== $password) {
+            return $this->render('login', ['messages' => ['Wrong password!']]);
+        }
+
+        $url = "http://$_SERVER[HTTP_HOST]";
+        header("Location: {$url}/tasks");
     }
 
     public function createAccount()
